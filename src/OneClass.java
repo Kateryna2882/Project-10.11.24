aa4b09f4d2d9abe930e3227229b826191b2e381b
Kateryna2882 <govoryhina82@gmail.com>
2024-11-10 18:37:23 +0200

    add fields

--- src/OneClass.java
+++ src/OneClass.java
@@ -1,5 +1,21 @@
 public class OneClass {
     public static void main(String[] args) {
+        int[][] binary = {
+                {0, 1, 0, 1},
+                {1, 1, 1, 0},
+                {1, 0, 0, 1},
+                {1, 0, 1, 0}
+        };
+        int onesCount = 0;
+        System.out.println(onesCount);
+
+        String[][] wordData = {{"study", "consider", "examine", "learn"}, {"ponder", "read", "think", "cogitate"}};
+
+        int i = 0;
+        int j = 0;
+
+        //System.out.println(wordData[i][j] + ": [" + i + "]" + "[" + j + "]");
+
 
     }
 }
