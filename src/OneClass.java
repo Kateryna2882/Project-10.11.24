52b5bebb934968df46a26ab5d6e03e7fb3f26519
Kateryna2882 <govoryhina82@gmail.com>
2024-11-10 18:46:04 +0200

    add fields and methods

--- src/OneClass.java
+++ src/OneClass.java
@@ -6,16 +6,29 @@ public class OneClass {
                 {1, 0, 0, 1},
                 {1, 0, 1, 0}
         };
+
         int onesCount = 0;
+
+        for (int[] row : binary) {
+            for (int i: row) {
+                onesCount += i;
+            }
+        }
+
+
         System.out.println(onesCount);
 
         String[][] wordData = {{"study", "consider", "examine", "learn"}, {"ponder", "read", "think", "cogitate"}};
 
-        int i = 0;
-        int j = 0;
-
-        //System.out.println(wordData[i][j] + ": [" + i + "]" + "[" + j + "]");
-
+        int i = 0, j = 0;
+        while (i < wordData.length) {
+            j = 0;
+            while (j < wordData[i].length) {
+                System.out.println(wordData[i][j] + ": [" + i + "]" + "[" + j + "]");
+                j++;
+            }
+            i++;
+        }
 
     }
 }
